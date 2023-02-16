0e37f6da65d973dc3f0320f21f4cd8ed45b96a66
Ben-A-Sabu <benasabu123@gmail.com>
2023-02-17 00:41:06 +0530

    version 1.0

--- ex.js
+++ ex.js
@@ -1,42 +1,57 @@
 const homebtn = document.querySelector('#homebtn');
+///////////////////////////////////////////////////////////////////////
 const Inputbox = document.querySelector('#taskEnter');
 const Taskdate = document.querySelector('#TaskDate');
 const Addbtn = document.querySelector('#Addbtn');
 const Clearbtn = document.querySelector('#Clearbtn');
+///////////////////////////////////////////////////////////////////////
 const tasks = document.querySelector('.tasks');
+///////////////////////// current task elements ///////////////////////////////////////////
 const today = document.querySelector('#today');
+//////////////////// upcoming task element /////////////////////////////////////////////////
 const upcoming = document.querySelector('#upcoming');
-const tasksList = document.querySelector('.task');
-const taskList1 = document.querySelector('.upcomming_task');
+///////////////////////completed task elements ///////////////////////////////////////////
+const completedTask = document.querySelector('#completed');
+////////////////////////missed task elements ////////////////////////////////////////////
+const Missed = document.querySelector('#Missed');
+/////////////////////// footer btn elements /////////////////////////////////////////////
 const viewMisedTasks = document.querySelector('#view_miss_task');
 const viewCompletedTasks = document.querySelector('#view_comp_task');
 const viewCurrentTasks = document.querySelector('#view_current_task');
-const completedTask = document.querySelector('#completed');
-const compTasklist = document.querySelector('.Ctask');
-const missedTaskList = document.querySelector('.Mtask');
-const Missed = document.querySelector('#Missed');
-
-
+///////////// global variable ///////////////////////////////////////////////////////////
 var currentdate;
 var month;
 var id = 0;
 var lastId = 0;
+/// event listeners used in the code
+window.addEventListener('load', function () {
 
-displayFromLocalStorage();
-findcurrentdate();
-updateNotification();
+    homebtn.addEventListener('click', function () {
+        window.location.href = "ex.html";
+    });
+    Clearbtn.addEventListener('click', function () {
+        Inputbox.value = '';
+        Taskdate.value = '';
+    });
+    viewMisedTasks.addEventListener('click', function () {
+        veiw_and_Hide_Area(today, upcoming, completedTask, Missed);
+    });
+    viewCompletedTasks.addEventListener('click', function () {
+        veiw_and_Hide_Area(today, upcoming, Missed, completedTask);
+    });
+    viewCurrentTasks.addEventListener('click', function () {
+        veiw_and_Hide_Area(Missed, upcoming, completedTask, today);
+    });
 
-console.log(localStorage.getItem('lastId'));
+    id = JSON.parse(localStorage.getItem('lastId')) || 0;
 
-if (localStorage.getItem('lastId') != null) {
+    displayFromLocalStorage();
+    findcurrentdate();
+    updateNotification();
 
-    id = localStorage.getItem('lastId');
-    id = JSON.parse(id);
+});
 
-} else {
-    id = 0;
-}
-
+// function to displa current date
 function findcurrentdate() {
     if (new Date().getMonth() + 1 < 10) {
         currentdate = new Date().getFullYear() + '-' + "0" + (new Date().getMonth() + 1) + '-' + new Date().getDate();
@@ -44,138 +59,71 @@ function findcurrentdate() {
         currentdate = new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate();
     }
 }
-
-
-homebtn.addEventListener('click', function () {
-
-    console.log("home button clicked");
-    window.location.href = "ex.html";
-});
-
-
-
-function addTask(task, date) {
-    let taskElement = document.createElement('li');
+/// function to add task on to screen
+function addTask(task, date, where_to_add) {
+    let taskElement = document.createElement('div');
     taskElement.classList.add('task');
-    taskElement.innerHTML = `<p >${task}</p>
-        <p>${date}</p><button class="action" id="deletebtn">delete</button><button class="action" id="editbtn">edit</button><button class="action" id="cmpltbtn">Done</button></div>`;
-    tasksList.appendChild(taskElement);
+    taskElement.innerHTML = `<div class="todo">${task}</div>
+        <div>${date}</div><button class="action" id="deletebtn">delete</button><button class="action" id="editbtn">edit</button><button class="action" id="cmpltbtn">Done</button>`;
     taskElement.setAttribute('id', id);
-    today.appendChild(taskElement);
+    where_to_add.appendChild(taskElement);
     AddToLocalStorage(task, date, id);
     updateid();
-    return taskElement;
+
 }
+// function to provide unique id for each task
+function updateid() {
+    id++;
+    lastId = id;
+    localStorage.setItem('lastId', JSON.stringify(lastId));
 
-
+}
+/// function to add task
 Addbtn.addEventListener('click', function () {
     let task = Inputbox.value;
     let date = Taskdate.value;
-    console.log(date);
 
     if (task === '' || date === '') {
         alert('Please enter a task and date');
     } else if (currentdate === date) {
-
-        let taskList = document.createElement('li');
-        taskList.classList.add('task');
-        taskList.innerHTML = `<p >${task}</p>
-        <p>${date}</p><button class="action" id="deletebtn">delete</button><button class="action" id="editbtn">edit</button><button class="action" id="cmpltbtn">Done</button></div>`;
-        tasksList.appendChild(taskList);
-        taskList.setAttribute('id', id);
-        today.appendChild(taskList);
-        AddToLocalStorage(task, date, id);
-        updateid();
-
+        addTask(task, date, today);
 
     } else if (new Date().getTime() < new Date(date).getTime()) {
-        let tasklist1 = document.createElement('li');
-        tasklist1.classList.add('upcomming_task');
-        tasklist1.innerHTML = `<p>${task}</p>
-        <p>${date}</p><button class="action" id="deletebtn">delete</button><button class="action" id="editbtn">edit</button><button class="action" id="cmpltbtn">Done</button></div>`;
-        taskList1.appendChild(tasklist1);
-        tasklist1.setAttribute('id', id);
-        upcoming.appendChild(tasklist1);
-        AddToLocalStorage(task, date, id);
-        updateid();
-
-
+        addTask(task, date, upcoming);
     } else {
         alert('Please enter a valid date');
     }
-
     Inputbox.value = '';
     Taskdate.value = '';
 
 });
-
-
-
-
-Clearbtn.addEventListener('click', function () {
-    Inputbox.value = '';
-    Taskdate.value = '';
-});
-
-
+/// action that can be performed (ie, edit, delete, complete)
 tasks.addEventListener('click', function (e) {
-
     if (e.target.id === 'deletebtn') {
-        console.log(e.target.parentElement.id);
         RemoveFromLocalStorage(e.target.parentElement.id);
-        e.target.parentElement.remove();
-
+        displayFromLocalStorage();
+        updateNotification();
     }
-
     if (e.target.id === 'editbtn') {
-        console.log(e.target.parentElement.id);
-        let prevdate = e.target.parentElement.querySelector('p:nth-child(2)').innerHTML;
-
         let editTask = prompt('Edit Task');
         let editDate = prompt('Edit Date' + '(' + 'YYYY-MM-DD' + ')');
-
-
-        e.target.parentElement.setAttribute('id', e.target.parentElement.id);
-        var value = validate_edited_task(editTask, editDate, e.target.parentElement.id, prevdate);
+        var value = validate_edited_task(editTask, editDate);
         if (value == true) {
             editFromLocalStorage(editTask, editDate, e.target.parentElement.id);
-            e.target.parentElement.innerHTML = `<p>${editTask}</p><p>${editDate}</p><button class="action" id="deletebtn">delete</button><button class="action" id="editbtn">edit</button><button class="action" id="cmpltbtn">Done</button></div>`;
-        } else {
-            alert('Please enter in valid format');
+            location.reload();
         }
-
-
-
     }
-
     if (e.target.id === 'cmpltbtn') {
-        console.log(e.target.parentElement.id);
         updateStatus(e.target.parentElement.id, 'completed');
-        e.target.parentElement.remove();
-        displayFromLocalStorage();
+        location.reload();
     }
-
     if (e.target.id == 'addbackbtn') {
-
         updateStatus(e.target.parentElement.id, 'notdone');
-        e.target.parentElement.remove();
-        displayFromLocalStorage();
-
+        location.reload();
     }
 
 });
-//console.log(localStorage.getItem('tasks'));
-
-function updateid() {
-    console.log("id being updated");
-    id++;
-    lastId = id;
-    console.log("updated id is " + lastId);
-    localStorage.setItem('lastId', JSON.stringify(lastId));
-
-}
-
-
+//// add to local storage function 
 function AddToLocalStorage(task, date, id) {
     let taskObj = {
         task: task,
@@ -183,246 +131,107 @@ function AddToLocalStorage(task, date, id) {
         id: id,
         taskstatus: 'notdone'
     };
-    let tasks = localStorage.getItem('tasks');
-
-    if (tasks == null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(tasks);
-    }
-
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     tasks.push(taskObj);
-    updateNotification();
-
     localStorage.setItem('tasks', JSON.stringify(tasks));
-
 }
-
-
-
+// function to create task element from local storage
+function createtasktoDisplay(taskname, date, task_id, where_to_add) {
+    let taskElement = document.createElement('div');
+    taskElement.classList.add('task');
+    if (where_to_add == completedTask) {
+        taskElement.innerHTML = `<div class="todo" >${taskname}</div>
+        <div>${date}</div><button class="action" id="deletebtn">delete</button><button class="action" id="editbtn">edit</button><button class="action" id="addbackbtn">Add Back</button>`;
+    }
+    else {
+        taskElement.innerHTML = `<div class="todo" >${taskname}</div>
+    <div>${date}</div><button class="action" id="deletebtn">delete</button><button class="action" id="editbtn">edit</button><button class="action" id="cmpltbtn">Done</button>`;
+    }
+    taskElement.setAttribute('id', task_id);
+    where_to_add.appendChild(taskElement);
+}
+//display from  local storage function
 function displayFromLocalStorage() {
 
     findcurrentdate();
-
-    let tasks = localStorage.getItem('tasks');
-
-    if (tasks === null) {
-        console.log('No tasks to display');
-        tasks = [];
-    } else {
-        tasks = JSON.parse(tasks);
-    }
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
     tasks.forEach(function (task) {
 
         if (new Date().getTime() < new Date(task.date).getTime() && task.taskstatus == 'notdone') {
-            let tasklist1 = document.createElement('li');
-            tasklist1.classList.add('upcomming_task');
-            tasklist1.innerHTML = `<p>${task.task}</p>
-            <p>${task.date}</p><button class="action" id="deletebtn">delete</button><button class="action" id="editbtn">edit</button><button class="action" id="cmpltbtn">Done</button></div>`;
-            taskList1.appendChild(tasklist1);
-            tasklist1.setAttribute('id', task.id);
-            upcoming.appendChild(tasklist1);
+            createtasktoDisplay(task.task, task.date, task.id, upcoming);
         } else if (currentdate == task.date && task.taskstatus == 'notdone') {
-            let taskList = document.createElement('li');
-            taskList.classList.add('task');
-            taskList.innerHTML = `<p >${task.task}</p>
-            <p>${task.date}</p><button class="action" id="deletebtn">delete</button><button class="action" id="editbtn">edit</button><button class="action" id="cmpltbtn">Done</button></div>`;
-            tasksList.appendChild(taskList);
-            taskList.setAttribute('id', task.id);
-            today.appendChild(taskList);
+            createtasktoDisplay(task.task, task.date, task.id, today);
         } else if (new Date().getTime() > new Date(task.date).getTime() && task.taskstatus == 'notdone' && currentdate != task.date) {
-            let taskList = document.createElement('li');
-            taskList.classList.add('Mtask');
-            taskList.innerHTML = `<p >${task.task}</p>
-            <p>${task.date}</p><button class="action" id="deletebtn">delete</button><button class="action" id="editbtn">edit</button><button class="action" id="cmpltbtn">Done</button></div>`;
-            missedTaskList.appendChild(taskList);
-            taskList.setAttribute('id', task.id);
-            Missed.appendChild(taskList);
+            createtasktoDisplay(task.task, task.date, task.id, Missed);
         } else if (task.taskstatus == 'completed' && (new Date().getTime() < new Date(task.date).getTime() || currentdate == task.date)) {
-            console.log("task is done");
-            let taskList = document.createElement('li');
-            taskList.classList.add('Ctask');
-            taskList.innerHTML = `<p >${task.task}</p><p>${task.date}</p></p><button class="action" id="addbackbtn">Add Back</button><button class="action" id="editbtn">edit</button>`;
-            compTasklist.appendChild(taskList);
-            taskList.setAttribute('id', task.id);
-            completedTask.appendChild(taskList);
+            createtasktoDisplay(task.task, task.date, task.id, completedTask);
         } else {
             RemoveFromLocalStorage(task.id);
         }
-
-
     });
 
-    updateNotification();
 }
-
-
-
+// edit from local storage function
 function editFromLocalStorage(task, date, id) {
-    let tasks = localStorage.getItem('tasks');
-    if (tasks == null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(tasks);
-    }
-
-
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
     for (let i = 0; i < tasks.length; i++) {
         if (tasks[i].id == id) {
             tasks[i].task = task;
             tasks[i].date = date;
-            console.log("haiiiiiiiiiiii");
         }
-
-
     }
-
-
     localStorage.setItem('tasks', JSON.stringify(tasks));
+    location.reload();
 }
-
+// remove from local storage function
 function RemoveFromLocalStorage(id) {
-    console.log(id);
-    let tasks = localStorage.getItem('tasks');
-    if (tasks === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(tasks);
-
-        console.log("task is being removed");
-
-        for (let i = 0; i < tasks.length; i++) {
-            if (tasks[i].id == id) {
-                console.log(id)
-                console.log(tasks[i].id)
-                tasks.splice(i, 1);
-
-                console.log("hauiiii");
-            }
-
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].id == id) {
+            console.log(id)
+            console.log(tasks[i].id)
+            tasks.splice(i, 1);
         }
     }
     localStorage.setItem('tasks', JSON.stringify(tasks));
-}
 
-function validate_edited_task(task, date, cid, prevdate) {
+}
+// validate_edited info function
+function validate_edited_task(task, date) {
     if (task == '' || date == '') {
         alert('Please enter a valid task and date');
         return false;
-    } else if (currentdate == date) {
-
-
-        if (prevdate != date) {
-            let taskList = document.createElement('li');
-            taskList.classList.add('task');
-            taskList.innerHTML = `<p >${task}</p>
-           <p>${date}</p><button class="action" id="deletebtn">delete</button><button class="action" id="editbtn">edit</button><button class="action" id="cmpltbtn">Done</button></div>`;
-            tasksList.appendChild(taskList);
-            taskList.setAttribute('id', cid);
-            today.appendChild(taskList);
-            RemoveFromLocalStorage(cid);
-            AddToLocalStorage(task, date, cid);
-            window.location.reload();
-
-        }
-
+    } else if (currentdate == date || new Date().getTime() < new Date(date).getTime()) {
         return true;
-
-    } else if (new Date().getTime() < new Date(date).getTime()) {
-
-        if (prevdate != date) {
-            let tasklist1 = document.createElement('li');
-            tasklist1.classList.add('upcomming_task');
-            tasklist1.innerHTML = `<p>${task}</p>
-            <p>${date}</p><button class="action" id="deletebtn">delete</button><button class="action" id="editbtn">edit</button><button class="action" id="cmpltbtn">Done</button></div>`;
-            taskList1.appendChild(tasklist1);
-            tasklist1.setAttribute('id', cid);
-            upcoming.appendChild(tasklist1);
-            RemoveFromLocalStorage(cid);
-            AddToLocalStorage(task, date, cid);
-            window.location.reload();
-        }
-
-        return true;
-
-    } else {
+    }
+    else {
         alert('Please enter a valid date');
         return false;
     }
-
-
-
 }
-
+// update status function
 function updateStatus(id, status) {
-    let tasks = localStorage.getItem('tasks');
-    if (tasks == null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(tasks);
-    }
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
     for (let i = 0; i < tasks.length; i++) {
         if (tasks[i].id == id) {
             tasks[i].taskstatus = status;
         }
     }
-
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
-
-
-
-viewMisedTasks.addEventListener('click', function () {
-
-    console.log("button clicked");
-    console.log(Missed);
-
-    today.style.display = 'none';
-    upcoming.style.display = 'none';
-    completedTask.style.display = 'none';
-    Missed.style.display = 'block';
-
-});
-
-viewCompletedTasks.addEventListener('click', function () {
-
-    console.log("button clicked");
-    console.log(completedTask);
-
-    today.style.display = 'none';
-    upcoming.style.display = 'none';
-    Missed.style.display = 'none';
-    completedTask.style.display = 'block';
-
-});
-
-
-viewCurrentTasks.addEventListener('click', function () {
-
-    console.log("button clicked");
-    console.log(today);
-
-    Missed.style.display = 'none';
-    upcoming.style.display = 'none';
-    completedTask.style.display = 'none';
-    today.style.display = 'block';
-
-});
-
-
-
-
+// veiew_and_Hide_Area function
+function veiw_and_Hide_Area(area1, area2, area3, area4) {
+    area1.style.display = 'none';
+    area2.style.display = 'none';
+    area3.style.display = 'none';
+    area4.style.display = 'block';
+}
+///  function updates number of task missed,completed, todo
 function updateNotification() {
-    let tasks = localStorage.getItem('tasks');
-    if (tasks == null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(tasks);
-    }
-
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     let No_of_task_completed = 0;
     let No_of_task_missed = 0;
     let No_of_task_todo = 0;
@@ -431,23 +240,20 @@ function updateNotification() {
         if (tasks[i].taskstatus == 'completed') {
             No_of_task_completed++;
         } else if (tasks[i].taskstatus == 'notdone') {
-
             if (new Date().getTime() < new Date(tasks[i].date).getTime() || currentdate == tasks[i].date) {
 
                 No_of_task_todo++;
             } else {
                 No_of_task_missed++;
             }
-
         }
     }
-
     count_completed_tasks = document.querySelector('#completedtask');
     count_missed_tasks = document.querySelector('#pending');
     count_current_tasks = document.querySelector('#tocomplete');
-
     count_completed_tasks.innerText = "Completed Task:" + No_of_task_completed;
     count_missed_tasks.innerText = "Missed Task:" + No_of_task_missed;
     count_current_tasks.innerText = "Current Task:" + No_of_task_todo;
-
 }
+
+
